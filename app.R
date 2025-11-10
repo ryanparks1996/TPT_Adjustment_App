7c582b275c72bf703628fc7d1e1589cbca174c06
Ryan Parks <ryan.j.parks1996@gmail.com>
2025-11-10 07:38:53 -0500

    added main app, helper file and game metric report
    The main app calls on functions in the helpers file to execute certain tasks. The Game Metric Report file can be downloaded so anyone can test out the application.

---
+++ app.R
@@ -0,0 +1,174 @@
+# Load in libraries
+library(shiny)
+library(bslib)
+library(tools)
+library(readxl)
+library(dplyr)
+
+# Load helper functions
+source('helpers.R')
+
+# List of desired column names. Will be used by removeExtraHeaders()
+desired_col_name <- c("Profile", "Game", "Credits",	"MINUTES", "TPT")
+
+# Define UI for application that takes in an .xslx file, creates some summary
+# tables and allows user to create a theoretical store tpt
+ui <- fluidPage(
+  
+  # App title 
+  titlePanel("TPT Adjustment Calculator"),
+  
+  # Input: Select a file 
+  fileInput(
+    "upload",
+    "Upload weekly TPT report",
+    multiple = FALSE,
+    accept = c(
+      ".xlsx"
+    )
+  ),
+  
+  fluidRow(
+    
+    # Output: summary statistics
+    column(width = 4,
+           tableOutput("report"),
+    ),
+    
+    # Adjusting store tpt by selecting game and typing in desired tpt
+    # for that game
+    column(width = 5,
+           helpText("Select a game you want to adjust. Then type new tpt",
+                    "value. The program will compute a new theoretical store average"),
+           
+           selectInput("gametoadjust",
+                       "Select game",
+                       choices = NULL,
+                       multiple = FALSE),
+           card(
+             card_header("Current TPT:"),
+             textOutput("currenttpt")
+           ),
+           
+           numericInput("newtpt",
+                        "New TPT",
+                        value = NULL,
+                        width = '33%',
+                        min = 0,
+                        max = 6,
+                        step = .1)
+    ),
+    
+    column(width = 3,
+           card(
+             card_header("New Theoretical TPT:"),
+             textOutput("theoreticaltpt"))
+    )
+  ),
+  
+  # Output: Top 5 Highest and Top 5 Lowest TPT
+  fluidRow(
+    column(width = 6,
+           helpText("Top 5 Highest and Top 5 Lowest TPT"),
+           tableOutput("badtpts")
+    ),
+    # Output: Top 10 Games With Largest Impact on Average TPT
+    column(width = 6,
+           helpText("Top 10 Games With Largest Impact on Average TPT"),
+           tableOutput("badtptsweighted")
+    )
+  )
+)
+
+# Define server logic
+server <- function(input, output) {
+  
+  data <- reactive({
+    req(input$upload)
+    filepath <- input$upload$datapath
+    dataset <- removeExtraHeaders(filepath, 
+                                  desired_col = desired_col_name,
+                                  rm_sums_row = TRUE)
+    # Add weighted TPT column to data frame (weighted by times played)
+    grand_total_plays <- sum(dataset$`Total Plays`)
+    dataset <- dataset %>%
+      mutate(`Weighted TPT` = abs((TPT - mean(TPT)) * 
+                                    (`Total Plays` / grand_total_plays)))
+  })
+  
+  # Update game choices for adjusting tpt
+  observeEvent(data(), {
+    choices <- data() %>%
+      arrange(desc(`Weighted TPT`)) %>%
+      select(Game)
+    updateSelectInput(inputId = "gametoadjust", choices = choices)
+  })
+  
+  # Output: display current tpt of selected game
+  output$currenttpt <- renderText({
+    req(input$gametoadjust)
+    
+    selected_game_tpt <- data()[data()$Game == input$gametoadjust, ]$TPT
+    return(selected_game_tpt)
+  })
+  
+  # Output: new theoretical tpt based on user input
+  output$theoreticaltpt <- renderText({
+    req(input$gametoadjust)
+    req(input$newtpt)
+    
+    new_data <- data()
+    
+    # Adjust game tpt to user selected value
+    game <- new_data[new_data$Game == input$gametoadjust, ]$Game
+    
+    new_data[new_data$Game == game, "TICKETS"] <- 
+      new_data[new_data$Game == game, "Total Plays"] * input$newtpt
+    
+    new_store_tpt <- round(tpt_summary(new_data)$values[6],3)
+    return(new_store_tpt)
+  })
+  
+  # Output: summary statistics table 
+  output$report <- renderTable({
+    
+    return(tpt_summary(data()))
+  }, rownames = TRUE, colnames = FALSE)
+  
+  # Output: table of most extreme tpts for redemption games
+  output$badtpts <- renderTable({
+    
+    greatest_tpt <- data() %>%
+      select(Game, TPT, `Total Plays`) %>%
+      arrange(desc(TPT)) %>%
+      head(5)
+    smallest_tpt <- data() %>%
+      filter(TicketProfile == "Redemption") %>%
+      select(Game, TPT, `Total Plays`) %>%
+      arrange(TPT) %>%
+      head(5)
+    
+    return(bind_rows(greatest_tpt, smallest_tpt))
+  })
+  
+  # Output: table of most extreme weighted tpts for redemption games
+  # Weighted by times played
+  output$badtptsweighted <- renderTable({
+    
+    greatest_weighted_tpt <- data() %>%
+      select(Game, TPT, `Total Plays`, `Weighted TPT`) %>%
+      arrange(desc(`Weighted TPT`)) %>%
+      head(5)
+    smallest_weighted_tpt <- data() %>%
+      filter(TicketProfile == "Redemption" & TPT < 3) %>%
+      select(Game, TPT, `Total Plays`, `Weighted TPT`) %>%
+      arrange(desc(`Weighted TPT`)) %>%
+      head(5)
+    
+    #return(greatest_weighted_tpt)
+    return(bind_rows(greatest_weighted_tpt, smallest_weighted_tpt))
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
---
+++ helpers.R
@@ -0,0 +1,104 @@
+library(readxl)
+library(dplyr)
+
+
+
+# FUNCTION TO PARSE DATA AND REMOVE EXTRA HEADERS ------------------------------
+# file: path to an excel file
+# desired_col: vector of some or all of the desired column names
+# rm_sums_row: will remove the last row (use when last row is a grand total)
+# rm_sums_col: will remove the last columns (use when last column is a grand total)
+
+removeExtraHeaders <- function(file, 
+                               desired_col, 
+                               rm_sums_row = FALSE,
+                               rm_sums_col = FALSE) {
+  # Read first few rows to check for correct header
+  data <- read_excel(file, n_max = 11) 
+  
+  # short loop to find what row the col_names are on
+  skip <- 1
+  desired_col_name <- rep(" ", ncol(data))
+  
+  for (row in 1:10) {
+    if (any(data[row, ] %in% desired_col)) {
+      skip <- row + 1 
+      desired_col_name <- data[row, ] %>%
+        as.character() # Convert to character vector
+      break # End the loop. We have found the proper header
+    }
+    # Return error if no match is found
+    if (row == 10) { # row = 10 only if no row including the 10th did not match
+      stop("No match was found. Unable to remove extra headers.")
+    }
+  }
+  
+  # Re-read the data but now filtering out the extra headers
+  data_corrected <- read_excel(file,
+                               skip = skip,
+                               col_names = desired_col_name)
+  
+  # Get number of rows and columns in corrected data frame
+  n <- nrow(data_corrected)
+  m <- ncol(data_corrected)
+  
+  # If desired, remove last row from data frame
+  if (rm_sums_row) {
+    data_corrected <- data_corrected[-n, ]
+  }
+  
+  # If desired, remove last column from data frame
+  if (rm_sums_col) {
+    data_corrected <- data_corrected[ , -m]
+  }
+  
+  # Return data frame with the proper header
+  return(data_corrected)
+}
+##----------------------------------------------------------------------------##
+
+# FUNCTION TO CREATE CUSTOM SUMAMRY STATISTICS TABLE
+# data: data frame of weekly tpt report
+tpt_summary <- function(data) {
+  # Total games
+  total_games <- nrow(data)
+  # Total non-redemption games
+  total_by_redemption <- data %>%
+    group_by(TicketProfile) %>%
+    summarize(num_of_games = n_distinct(Game))
+  # Number of games above 6 TPT
+  n_tpt_above_6 <- data %>%
+    filter(TPT > 6) %>%
+    summarize(n_distinct(Game)) %>%
+    as.numeric()
+  # Number of redemption games below 2 TPT
+  n_tpt_below_2 <- data %>%
+    filter((TicketProfile == "Redemption") & (TPT < 2)) %>%
+    summarize(n_distinct(Game)) %>%
+    as.numeric()
+  # Overall TPT
+  tpt_all <- sum(data$TICKETS) / sum(data$`Total Plays`)
+
+  
+  
+  row_names <- c("Total Games:", 
+            "Redemption Games:", 
+            "Non-Redemption Games:",
+            "Games (TPT>6):",
+            "Games (TPT<2):",
+            "Overall TPT:")
+  
+  column <- c(total_games, 
+            as.numeric(total_by_redemption[2, 2]), 
+            as.numeric(total_by_redemption[1, 2]),
+            n_tpt_above_6,
+            n_tpt_below_2,
+            tpt_all)
+  
+  tpt_table <- data.frame("values" = column,
+                          row.names = row_names)
+  return(tpt_table)
+}
+##----------------------------------------------------------------------------##
+
+
